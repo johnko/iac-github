d5d33616adc1ef5b8be415e674b43df6b49187f3
johnko <279736+johnko@users.noreply.github.com>
2025-11-06 06:49:12 -0500

    shfmt

--- github/johnko/import.sh
+++ github/johnko/import.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 
 $IAC_BIN state show 'github_repository.archived["deploy"]' ||
-    $IAC_BIN import 'github_repository.archived["deploy"]' deploy
+  $IAC_BIN import 'github_repository.archived["deploy"]' deploy
 
 $IAC_BIN state show 'github_repository.active["homedir"]' ||
-    $IAC_BIN import 'github_repository.active["homedir"]' homedir
+  $IAC_BIN import 'github_repository.active["homedir"]' homedir
 
 $IAC_BIN state show 'github_repository.active["renovate-config"]' ||
-    $IAC_BIN import 'github_repository.active["renovate-config"]' renovate-config
+  $IAC_BIN import 'github_repository.active["renovate-config"]' renovate-config
