54b6b553c76a7baaa87a1a89689225458ffd9c09
johnko <279736+johnko@users.noreply.github.com>
2025-11-06 01:25:56 -0500

    install shfmt in CI

--- .github/shfmt.sh
+++ .github/shfmt.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -exo pipefail
+
+# when running in CI and shfmt doesn't exist, install it
+if [[ -n "$CI" ]]; then
+  if ! type shfmt &>/dev/null ; then
+    if uname -a | grep -i -q 'notfoundnotfoundnotfoundnotfoundnotfound' ; then
+      apt install --yes shfmt
+    else
+      go install mvdan.cc/sh/v3/cmd/shfmt@3.11.0
+    fi
+  fi
+fi
+set -u
 
 shfmt --diff --simplify --indent 2 --case-indent ./
 
