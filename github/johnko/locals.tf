7a3db70931418fc991a99f28bca8387fa7acd54f
johnko <279736+johnko@users.noreply.github.com>
2025-11-06 06:52:03 -0500

    organize

--- github/johnko/locals.tf
+++ github/johnko/locals.tf
@@ -1,42 +1,4 @@
 locals {
-  default_repo_settings = {
-    # name = ""
-    description  = ""
-    homepage_url = ""
-    visibility   = "private"
-
-    has_issues      = false
-    has_discussions = false
-    has_projects    = false
-    has_wiki        = false
-    is_template     = false
-
-    allow_merge_commit          = false
-    allow_squash_merge          = true
-    allow_rebase_merge          = true
-    allow_auto_merge            = false
-    delete_branch_on_merge      = true
-    web_commit_signoff_required = false
-
-    auto_init = false
-
-    archived           = false
-    archive_on_destroy = true
-
-    security_and_analysis = {
-      advanced_security = {
-        status = "disabled" # disabled on personal plan
-      }
-      secret_scanning = {
-        status = "enabled"
-      }
-      secret_scanning_push_protection = {
-        status = "enabled"
-      }
-    }
-
-    allow_update_branch = true
-  }
   repos = {
     deploy = {
       archived     = true
@@ -53,13 +15,5 @@ locals {
       has_issues  = true
       visibility  = "public"
     }
-  }
-
-  all_repos_settings = { for k, v in local.repos : k => merge(local.default_repo_settings, v) }
-  active_repos_settings = { for k, v in local.all_repos_settings : k => v
-    if v.archived == false
-  }
-  archived_repos_settings = { for k, v in local.all_repos_settings : k => v
-    if v.archived == true
   }
 }
--- github/johnko/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "all_repos_settings" {
-  value = local.all_repos_settings
-}
-output "active_repos_settings" {
-  value = local.active_repos_settings
-}
-output "archived_repos_settings" {
-  value = local.archived_repos_settings
-}
