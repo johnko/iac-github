c0bcb918859b55c1405fe59d6af5b93f4d4bbe2c
johnko <279736+johnko@users.noreply.github.com>
2025-11-06 01:45:04 -0500

    try brew

--- .github/shfmt.sh
+++ .github/shfmt.sh
@@ -4,13 +4,7 @@ set -exo pipefail
 # when running in CI and shfmt doesn't exist, install it
 if [[ -n "$CI" ]]; then
   if ! type shfmt &>/dev/null ; then
-    if type apt &>/dev/null  ; then
-      SUDO=''
-      if type sudo &>/dev/null  ; then
-        SUDO=sudo
-      fi
-      apt install --yes shfmt || $SUDO apt install --yes shfmt
-    elif type brew &>/dev/null  ; then
+    if type brew &>/dev/null  ; then
       brew install shfmt
     elif type go &>/dev/null  ; then
       if [[ ! -d "$HOME/bin" ]]; then
@@ -20,6 +14,12 @@ if [[ -n "$CI" ]]; then
       export PATH="$GOBIN:$PATH"
       go install mvdan.cc/sh/v3/cmd/shfmt@v3.11.0
       ls -l $GOBIN/shfmt
+    elif type apt &>/dev/null  ; then
+      SUDO=''
+      if type sudo &>/dev/null  ; then
+        SUDO=sudo
+      fi
+      apt install --yes shfmt || $SUDO apt install --yes shfmt
     fi
   fi
 fi
