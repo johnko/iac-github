872dbad13c30f6cbbf5b5891e6e1128d04d4b44f
johnko <279736+johnko@users.noreply.github.com>
2025-11-08 02:47:02 -0500

    chore: track github_actions_secret.RENOVATE_PRIVATE_KEY

--- github/johnko/github-actions-sync.tf
+++ github/johnko/github-actions-sync.tf
@@ -46,11 +46,21 @@ resource "github_branch" "to_create" {
 variable "RENOVATE_APP_ID" {
   type = string
 }
-
 resource "github_actions_secret" "RENOVATE_APP_ID" {
   for_each = local.active_repos_settings
 
   repository      = github_repository.active[each.key].name
   secret_name     = "RENOVATE_APP_ID"
   plaintext_value = sensitive(var.RENOVATE_APP_ID)
+}
+
+variable "RENOVATE_PRIVATE_KEY" {
+  type = string
+}
+resource "github_actions_secret" "RENOVATE_PRIVATE_KEY" {
+  for_each = local.active_repos_settings
+
+  repository      = github_repository.active[each.key].name
+  secret_name     = "RENOVATE_PRIVATE_KEY"
+  plaintext_value = sensitive(var.RENOVATE_PRIVATE_KEY)
 }
