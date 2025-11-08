42c747ef4aab5b621c4b8845a386596fac94855c
johnko <279736+johnko@users.noreply.github.com>
2025-11-08 03:47:49 -0500

    chore: skip state rm labels

--- github/johnko/repos/import.sh
+++ github/johnko/repos/import.sh
@@ -4,35 +4,6 @@ set -euxo pipefail
 # shellcheck disable=SC1091
 source import-active-repos.sh
 
-LABELS="
-dependencies
-depName=ghcr.io/renovatebot/renovate
-depName=hashicorp/setup-terraform
-depName=hashicorp/terraform
-depName=opentofu/opentofu
-depName=opentofu/setup-opentofu
-depName=renovate
-depName=renovatebot/github-action
-major
-manager=github-actions
-manager=regex
-minor
-packageName=ghcr.io/renovatebot/renovate
-packageName=hashicorp/setup-terraform
-packageName=hashicorp/terraform
-packageName=opentofu/opentofu
-packageName=opentofu/setup-opentofu
-packageName=renovatebot/github-action
-patch
-"
-
-for i in $ACTIVE_REPOS; do
-  for l in $LABELS; do
-    $IAC_BIN state show "github_issue_label.active[\"$i-$l\"]" &&
-      $IAC_BIN state rm "github_issue_label.active[\"$i-$l\"]" || true
-  done
-done
-
 ARCHIVED_REPOS="
 deploy
 "
