16e6d0bc80105e300164fde971bdcaec4b003e8b
johnko <279736+johnko@users.noreply.github.com>
2025-11-08 16:59:54 -0500

    chore: unlink config.tf so renovatebot can upgrade

--- github/johnko/labels/config.tf
+++ github/johnko/labels/config.tf
@@ -1,1 +1,11 @@
-../repos/config.tf
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.7.3"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {}
