04661b9e22bad987dd119ae93cb25519403ddd6c
johnko <279736+johnko@users.noreply.github.com>
2025-11-08 23:30:06 -0500

    fix: remove branch import

--- github/johnko/repos/_import.sh
+++ github/johnko/repos/_import.sh
@@ -27,8 +27,3 @@ for i in $ACTIVE_REPOS; do
   $IAC_BIN state show "github_branch_default.active[\"$i\"]" ||
     $IAC_BIN import "github_branch_default.active[\"$i\"]" "$i"
 done
-
-for i in $ACTIVE_REPOS; do
-  $IAC_BIN state show "github_branch.to_create[\"$i\"]" ||
-    $IAC_BIN import "github_branch.to_create[\"$i\"]" "$i:github-actions-sync" || true
-done
